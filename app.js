f8630704fc2f171f383b24e921c101f9275744a1
Andrea Stagi <stagi.andrea@gmail.com>
2018-12-13 17:07:08 +0100

    Fix mongo connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const config = require('./db');
+const mongodb = require('mongodb')
 const client = mongodb.MongoClient;
 
 client.connect(config.DB, function (err, db) {
